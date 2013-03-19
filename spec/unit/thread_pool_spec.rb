ec3ba307f73c996d0c0c428ed04a3bb6de386ee2
Dr Moriarty <jamespaulmoriarty@gmail.com>
2013-03-19 19:02:37 +1100

    init

---
+++ spec/unit/thread_pool_spec.rb
@@ -0,0 +1,20 @@
+require_relative "../spec_helper"
+
+describe ThreadPool do
+
+  context "#schedule" do
+
+    it("execute jobs in parallel.") do
+      string = ""
+      thread_pool = ThreadPool.new(2)
+      thread_pool.schedule { sleep(4); string = "The 1st job." }
+      thread_pool.schedule { sleep(1); string = "The 2nd job." }
+      sleep(2)
+      string.should eq "The 2nd job."
+      sleep(3)
+      string.should eq "The 1st job."
+    end
+
+  end
+
+end
